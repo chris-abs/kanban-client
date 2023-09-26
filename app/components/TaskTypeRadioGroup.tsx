be9c71b0b4ee8ccb8d86f18661bf6d94b98aef4b
Chris Abbott <chrisabbott@Chriss-Mac-mini.local>
2023-09-26 19:18:27 +0100

    modal in progress

---
+++ app/components/TaskTypeRadioGroup.tsx
@@ -0,0 +1,26 @@
+const groupTypes = [
+  {
+    id: 'todo',
+    name: 'Todo',
+    desription: 'A task not started',
+    color: 'bg-red-500',
+  },
+  {
+    id: 'inprogress',
+    name: 'In Progress',
+    description: 'A task current in progress',
+    color: 'bg-amber_300',
+  },
+  {
+    id: 'done',
+    name: 'done',
+    description: 'A completed task',
+    color: 'bg-green-500',
+  },
+]
+
+const TaskTypeRadioGroup = () => {
+  return
+}
+
+export default TaskTypeRadioGroup
--- app/store/BoardStore.ts
+++ app/store/BoardStore.ts
@@ -6,6 +6,10 @@ interface BoardState {
   getBoard: () => void
   setBoardState: (board: Board) => void
   updateTodoInDB: (todo: Todo, columnId: TypedColumn) => void
+
+  newTaskInput: string
+  deleteTask: (taskIndex: number, todoId: Todo, id: TypedColumn) => void
+  setNewTaskInput: (input: string) => void
 }
 
 export const useBoardStore = create<BoardState>((set) => ({
@@ -23,4 +27,12 @@ export const useBoardStore = create<BoardState>((set) => ({
   updateTodoInDB: async (todo, columnId) => {
     // call to update in database
   },
+
+  newTaskInput: '',
+
+  deleteTask: async (taskIndex: number, todo: Todo, id: TypedColumn) => {
+    // call to delete in database
+  },
+
+  setNewTaskInput: (input: string) => set({ newTaskInput: input }),
 }))
